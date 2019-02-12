cf0b823f34b29651472aa75fc0c151b41408a29f
Shaun <shaun@thenetninja.co.uk>
2019-02-12 13:47:02 +0000

    lesson-20

--- chapter_three/sandbox.js
+++ chapter_three/sandbox.js
@@ -1,13 +1,14 @@
-// for loops
+// while loops
+const names = ['shaun', 'mario', 'luigi'];
+let i = 0;
 
-// for(let i = 0; i < 5; i++){
+// while(i < 5){
 //   console.log('loop: ', i);
+//   i++;
 // }
 
-const names = ['shaun', 'mario', 'luigi'];
-
-for(let i = 0; i < names.length; i++){
-  // console.log(names[i]);
-  let html = `<div>${names[i]}</div>`;
-  console.log(html);
+while(i < names.length){
+  console.log(names[i]);
+  i++;
 }
+
