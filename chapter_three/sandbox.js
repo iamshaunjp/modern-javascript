dfbd828a36451337f7ffc560f5e25d69d8ba51f5
Shaun <shaun@thenetninja.co.uk>
2019-02-12 16:28:47 +0000

    lesson-27

--- chapter_three/sandbox.js
+++ chapter_three/sandbox.js
@@ -1,17 +1,37 @@
-// break & continue
-const scores = [50, 25, 0, 30, 100, 20, 10];
+// switch statements
+const grade = 'D';
 
-for(let i = 0; i < scores.length; i++){
-
-  if(scores[i] === 0){
-    continue;
-  }
-
-  console.log('your score:', scores[i]);
-
-  if(scores[i] === 100){
-    console.log('congrats, you got the top score!');
+switch(grade){
+  case 'A':
+    console.log('you got an A!');
     break;
-  }
-
+  case 'B':
+    console.log('you got a B!');
+    break;
+  case 'C':
+    console.log('you got a C!');
+    break;
+  case 'D':
+    console.log('you got a D!');
+    break;
+  case 'E':
+    console.log('you got an E!');
+    break;
+  default:
+    console.log('not a valid grade');
 }
+
+// using if statements
+// if(grade === 'A'){
+
+// } else if(grade === 'B'){
+
+// } else if(grade === 'C'){
+
+// } else if(grade === 'D'){
+
+// } else if(grade === 'E'){
+
+// } else {
+
+// }
