59bd310e7dd2dd507285bc1b390277c688d1ffda
Shaun <shaun@thenetninja.co.uk>
2019-02-12 15:24:11 +0000

    lesson-23

--- chapter_three/sandbox.js
+++ chapter_three/sandbox.js
@@ -1,18 +1,10 @@
-// if statements
-// const age = 25;
+// else if statements
+const password = 'p@ssword123456';
 
-// if(age > 20){
-//   console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 3){
-//   console.log("that's a lot of ninjas!");
-// }
-
-const password = 'p@ssword';
-
-if(password.length >= 8){
+if(password.length >= 12){
+  console.log('that password is mighty strong');
+} else if(password.length >= 8){
   console.log('that password is long enough');
+} else {
+  console.log('password should be at least 8 characters long');
 }
