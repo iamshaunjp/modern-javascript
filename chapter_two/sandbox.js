35183c14b60ba8daaba9c69567b681a7fe7d7f1a
Shaun <shaun@thenetninja.co.uk>
2019-02-12 12:49:52 +0000

    lesson-17

--- chapter_two/sandbox.js
+++ chapter_two/sandbox.js
@@ -1,15 +1,15 @@
-let age = 25;
+// type conversion
+// let score = '100';
 
-// loose comparison (different types can still be equal)
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
 
-// console.log(age == 25);
-// console.log(age == '25');
-// console.log(age != 25);
-// console.log(age != '25');
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+let result = Boolean('');
 
-// strict comparison (different types cannot be equal)
-
-// console.log(age === 25);
-// console.log(age === '25');
-// console.log(age !== 25);
-// console.log(age !== '25');
+console.log(result, typeof result);
