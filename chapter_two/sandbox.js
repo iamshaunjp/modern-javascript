9ec6f63264ebb280f9a0e2b325e57b26841463c5
Shaun <shaun@thenetninja.co.uk>
2019-02-12 11:57:34 +0000

    lesson-14

--- chapter_two/sandbox.js
+++ chapter_two/sandbox.js
@@ -1,28 +1,7 @@
-// arrays
+// null & undefined
+let age = null;
 
-let ninjas = ['shaun', 'ryu', 'chun-li'];
-
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-
-// array length
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join(',');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.push('ken');
-let result = ninjas.pop();
-
-console.log(result);
-console.log(ninjas);
+console.log(age, age + 3, `the age is ${age}`);
 
 
 
