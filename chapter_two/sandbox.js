cd84c40d101e5d375ca0aca1864ebe61689155dd
Shaun <shaun@thenetninja.co.uk>
2019-02-12 10:50:16 +0000

    lesson-11

--- chapter_two/sandbox.js
+++ chapter_two/sandbox.js
@@ -1,16 +1,48 @@
-// common string methods
+// numbers
 
-let email = 'mario@thenetninja.co.uk';
+let radius = 10;
+let pi = 3.14;
 
-//let result = email.lastIndexOf('n');
+// console.log(radius, pi);
 
-//let result = email.slice(0,5);
+// math operators - +, -, *, /, **, %
 
-//let result = email.substr(5,12);
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2;
 
-//let result = email.replace('m', 'w');
+// order of operation - B I D M A S
 
-let result = email.replace('n', 'w');
+// let result = 5 * (10 - 3)**2;
 
+// console.log(result);
+
+// shorthands
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+
+// likes = likes + 10;
+// likes += 10;
+
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = 'the blog has ' + likes + ' likes.';
 console.log(result);
+
+
+
+
+
+
+
 
