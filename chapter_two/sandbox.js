fd44d9248ac9462c9e492febbcc84e2d9817b3c0
Shaun <shaun@thenetninja.co.uk>
2019-02-12 12:17:15 +0000

    lesson-15

--- chapter_two/sandbox.js
+++ chapter_two/sandbox.js
@@ -1,7 +1,37 @@
-// null & undefined
-let age = null;
+// booleans & comparisons
+// console.log(true, false, 'true', 'false');
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return booleans
+// let email = 'luigi@thenetninja.co.uk';
+// let names = ['mario', 'luigi', 'toad'];
+
+// let result = email.includes('@');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
+
+
+
+
 
 
 
