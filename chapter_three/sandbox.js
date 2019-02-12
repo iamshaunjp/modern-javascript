a1d4bc66627799602b216376b8b98769053f7e25
Shaun <shaun@thenetninja.co.uk>
2019-02-12 15:49:56 +0000

    lesson-25

--- chapter_three/sandbox.js
+++ chapter_three/sandbox.js
@@ -1,10 +1,10 @@
-// else if statements
-const password = 'p@ss12';
+// Logical NOT
+const user = false;
 
-if(password.length >= 12 && password.includes('@')){
-  console.log('that password is mighty strong');
-} else if(password.length >= 8 || password.includes('@') && password.length > 5){
-  console.log('that password is strong enough');
-} else {
-  console.log('that password is not strong enough');
+if(!user){
+  // do something
+  console.log('you must be logged in to continue');
 }
+
+console.log(!true);
+console.log(!false);
