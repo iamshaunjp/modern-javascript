bda9d915bf91056d05f20c855ac6a9bd2320e994
Shaun <shaun@thenetninja.co.uk>
2019-02-12 16:56:15 +0000

    lesson-30

---
+++ chapter_four/sandbox.js
@@ -0,0 +1,14 @@
+// function declaration
+function greet(){
+  console.log('hello there');
+}
+
+// function expression
+const speak = function(){
+  console.log('good day!');
+};
+
+// greet();
+// greet();
+
+speak();
