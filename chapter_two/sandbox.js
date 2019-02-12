b904f58edb15bca8158e4495fad868f9e0e237bb
Shaun <shaun@thenetninja.co.uk>
2019-02-12 09:21:31 +0000

    lesson-7

--- chapter_two/sandbox.js
+++ chapter_two/sandbox.js
@@ -1,2 +1,14 @@
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100;
+// points = 50;
+// console.log(points);
+
+// var score = 75;
+// console.log(score);
