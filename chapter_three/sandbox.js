be5bc48fb1e2ff065e2ace9ba5d9c3efd3763f44
Shaun <shaun@thenetninja.co.uk>
2019-02-12 15:19:21 +0000

    lesson-22

--- chapter_three/sandbox.js
+++ chapter_three/sandbox.js
@@ -1,8 +1,18 @@
-// do while loops
-let i = 5;
+// if statements
+// const age = 25;
 
-do{
-  console.log('val of i is: ', i);
-  i++;
-} while(i < 5);
+// if(age > 20){
+//   console.log('you are over 20 years old');
+// }
 
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//   console.log("that's a lot of ninjas!");
+// }
+
+const password = 'p@ssword';
+
+if(password.length >= 8){
+  console.log('that password is long enough');
+}
