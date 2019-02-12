bdbad720eec39c39d7ff56bc4712040fcb4c8e97
Shaun <shaun@thenetninja.co.uk>
2019-02-12 12:27:18 +0000

    lesson-16

--- chapter_two/sandbox.js
+++ chapter_two/sandbox.js
@@ -1,43 +1,15 @@
-// booleans & comparisons
-// console.log(true, false, 'true', 'false');
-
-// methods can return booleans
-// let email = 'luigi@thenetninja.co.uk';
-// let names = ['mario', 'luigi', 'toad'];
-
-// let result = email.includes('@');
-// let result = names.includes('luigi');
-
-// console.log(result);
-
-// comparison operators
 let age = 25;
 
-console.log(age == 25);
-console.log(age == 30);
-console.log(age != 30);
-console.log(age > 20);
-console.log(age < 20);
-console.log(age <= 25);
-console.log(age >= 25);
+// loose comparison (different types can still be equal)
 
-let name = 'shaun';
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
 
-console.log(name == 'shaun');
-console.log(name == 'Shaun');
-console.log(name > 'crystal');
-console.log(name > 'Shaun');
-console.log(name > 'Crystal');
+// strict comparison (different types cannot be equal)
 
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
