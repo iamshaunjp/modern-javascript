95655fc6fb0a6aa447952022016ff5d48ad6602e
Shaun <shaun@thenetninja.co.uk>
2019-02-12 15:33:00 +0000

    lesson-24

--- chapter_three/sandbox.js
+++ chapter_three/sandbox.js
@@ -1,10 +1,10 @@
 // else if statements
-const password = 'p@ssword123456';
+const password = 'p@ss12';
 
-if(password.length >= 12){
+if(password.length >= 12 && password.includes('@')){
   console.log('that password is mighty strong');
-} else if(password.length >= 8){
-  console.log('that password is long enough');
+} else if(password.length >= 8 || password.includes('@') && password.length > 5){
+  console.log('that password is strong enough');
 } else {
-  console.log('password should be at least 8 characters long');
+  console.log('that password is not strong enough');
 }
