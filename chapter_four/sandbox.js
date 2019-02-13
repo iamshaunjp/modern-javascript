d573f797c799d343ea1a43a2326b3c75731a59e3
Shaun <shaun@thenetninja.co.uk>
2019-02-13 09:52:45 +0000

    lesson-34

--- chapter_four/sandbox.js
+++ chapter_four/sandbox.js
@@ -1,39 +1,15 @@
-// arrow functions
+const name = 'shaun';
 
-// regular function
-// const calcArea = function(radius){
-//   return 3.14 * radius**2;
-// }
+// function
 
-// arrow function
-const calcArea = radius => 3.14 * radius**2;
+const greet = () => {
+  return 'hello';
+};
 
-const area = calcArea(5);
-console.log('area is:', area);
+let resultOne = greet();
+console.log(resultOne);
 
-// practise arrow functions
+// method
 
-// const greet = function(){
-//   return 'hello, world';
-// }
-
-const greet = () => 'hello, world';
-
-// const bill = function(products, tax){
-//   let total = 0;
-//   for(let i = 0; i < products.length; i++){
-//     total += products[i] + products[i] * tax;
-//   }
-//   return total;
-// }
-
-const bill = (products, tax) => {
-  let total = 0;
-  for(let i = 0; i < products.length; i++){
-    total += products[i]  + products[i] * tax;
-  }
-  return total;
-}
-
-console.log(greet());
-console.log(bill([10,15,30], 0.2));
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
