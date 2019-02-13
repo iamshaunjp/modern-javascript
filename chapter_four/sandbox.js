563c623bf1a4c99833a151e1487a86af9461c1e1
Shaun <shaun@thenetninja.co.uk>
2019-02-13 09:37:19 +0000

    lesson-33

--- chapter_four/sandbox.js
+++ chapter_four/sandbox.js
@@ -1,13 +1,39 @@
-// return statements
+// arrow functions
 
-// const speak = function(name, time){
-//   console.log(`good ${time}, ${name}!`);
-// };
+// regular function
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// }
 
-const calcArea = function(radius){
-  let area = 3.14 * radius**2;
-  return area;
-}
+// arrow function
+const calcArea = radius => 3.14 * radius**2;
 
 const area = calcArea(5);
 console.log('area is:', area);
+
+// practise arrow functions
+
+// const greet = function(){
+//   return 'hello, world';
+// }
+
+const greet = () => 'hello, world';
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i]  + products[i] * tax;
+  }
+  return total;
+}
+
+console.log(greet());
+console.log(bill([10,15,30], 0.2));
