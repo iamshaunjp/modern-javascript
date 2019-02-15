e3df599ab32a55fac7612b8679cdab5582650c4c
Shaun <shaun@thenetninja.co.uk>
2019-02-15 10:13:41 +0000

    lesson-36

--- chapter_four/sandbox.js
+++ chapter_four/sandbox.js
@@ -1,6 +1,13 @@
-// callbacks & forEach
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+const ul = document.querySelector('.people');
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
 
 people.forEach(person => {
-  console.log(`hello ${person}`);
+  // create html template for each person
+  html += `<li style="color: purple">${person}</li>`;
 });
+
+console.log(html);
+ul.innerHTML = html;
