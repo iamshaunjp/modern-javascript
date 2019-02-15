0ed709b76cd2a92aee7d6118bd3d07d188be6c8f
Shaun <shaun@thenetninja.co.uk>
2019-02-15 15:25:20 +0000

    lesson-51

--- chapter_six/sandbox.js
+++ chapter_six/sandbox.js
@@ -1,11 +1,15 @@
-const title = document.querySelector('h1');
+// const content = document.querySelector('p');
 
-// title.setAttribute('style', 'margin: 50px;');
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('success');
 
-console.log(title.style);
-console.log(title.style.color);
+const paras = document.querySelectorAll('p');
 
-title.style.margin = '50px';
-title.style.color = 'crimson';
-title.style.fontSize = '60px';
-title.style.margin = '';
+paras.forEach(p => {
+  if(p.textContent.includes('error')){
+    p.classList.add('error');
+  } else if(p.textContent.includes('success')) {
+    p.classList.add('success');
+  }
+})
