1b372195a2d89bc46396908cc60ddc080ec751ba
Shaun <shaun@thenetninja.co.uk>
2019-02-15 17:43:57 +0000

    lesson-55

--- chapter_six/sandbox.js
+++ chapter_six/sandbox.js
@@ -1,22 +1,28 @@
 const ul = document.querySelector('ul');
-// ul.remove();
-
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
   const li = document.createElement('li');
   li.textContent = 'something new to do';
-  //ul.appendChild(li);
-  ul.prepend(li);
+  ul.appendChild(li);  
 });
 
-const items = document.querySelectorAll('li');
+// const items = document.querySelectorAll('li');
 
-items.forEach(item => {
-  item.addEventListener('click', e => {
-    // e.target.style.textDecoration = 'line-through';
+// items.forEach(item => {
+//   item.addEventListener('click', e => {
+//     console.log('event in LI');
+//     e.stopPropagation();
+//     e.target.remove();
+//   });
+// });
+
+ul.addEventListener('click', e => {
+  // console.log('event in UL');
+  console.log(e.target, e);
+  if(e.target.tagName === 'LI'){
     e.target.remove();
-  });
+  }
 });
 
 
