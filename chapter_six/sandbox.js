fdb0140874671a5002eb75f12a3c5b51b6190a73
Shaun <shaun@thenetninja.co.uk>
2019-02-15 12:09:14 +0000

    lesson-47

--- chapter_six/sandbox.js
+++ chapter_six/sandbox.js
@@ -1,12 +1,13 @@
-// const para = document.querySelector('p');
-// const para = document.querySelector('.error');
-const para = document.querySelector('div.error');
+// get an element by ID
+const title= document.getElementById('page-title');
+console.log(title);
 
-console.log(para);
+// get elements by their class name
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+console.log(errors[0]);
 
-// query multiple elements at once
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
-
-console.log(paras, errors);
-console.log(paras[1], errors[0]);
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
