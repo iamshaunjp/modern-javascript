3e9e9b73dd496325e0c73939cbaeff42928d938f
Shaun <shaun@thenetninja.co.uk>
2019-02-15 17:10:11 +0000

    lesson-53

--- chapter_six/sandbox.js
+++ chapter_six/sandbox.js
@@ -1,22 +1,20 @@
-const article = document.querySelector('article');
+// const button = document.querySelector('button');
 
-// console.log(article.children);
-// console.log(Array.from(article.children));
-// console.log(article.children);
+// button.addEventListener('click', () => {
+//   console.log('you clicked me');
+// });
 
-Array.from(article.children).forEach(child => {
-  child.classList.add('article-element');
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+  item.addEventListener('click', e => {
+    // console.log('item clicked');
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(item);
+    e.target.style.textDecoration = 'line-through';
+  });
 });
-
-const title = document.querySelector('h2');
-
-console.log(title.parentElement);
-console.log(title.parentElement.parentElement);
-console.log(title.nextElementSibling);
-console.log(title.previousElementSibling);
-
-// chaining
-console.log(title.nextElementSibling.parentElement.children);
 
 
 
