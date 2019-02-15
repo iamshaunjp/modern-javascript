910990a263d99f5e8aa225d7065823cf688c595c
Shaun <shaun@thenetninja.co.uk>
2019-02-15 13:01:27 +0000

    lesson-50

--- chapter_six/sandbox.js
+++ chapter_six/sandbox.js
@@ -1,11 +1,11 @@
-const link = document.querySelector('a');
+const title = document.querySelector('h1');
 
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.thenetninja.co.uk');
-link.textContent = 'The Net Ninja webiste';
+// title.setAttribute('style', 'margin: 50px;');
 
-const mssg = document.querySelector('p');
+console.log(title.style);
+console.log(title.style.color);
 
-console.log(mssg.getAttribute('class'));
-mssg.setAttribute('class', 'success');
-mssg.setAttribute('style', 'color: green');
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+title.style.margin = '';
