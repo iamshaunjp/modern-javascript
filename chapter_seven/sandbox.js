26dcf8fdc56f009c985ae6d2fa2b15d233e0320b
Shaun <shaun@thenetninja.co.uk>
2019-02-16 09:45:54 +0000

    lesson-59

---
+++ chapter_seven/sandbox.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('.signup-form');
+// const username = document.querySelector('#username');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // console.log('form submitted');
+  // console.log(username.value);
+  console.log(form.username.value);
+});
