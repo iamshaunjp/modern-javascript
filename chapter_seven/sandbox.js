522f7e2b3be11af54519995fc6b7c140834ab23d
Shaun <shaun@thenetninja.co.uk>
2019-02-16 10:20:16 +0000

    lesson-62

--- chapter_seven/sandbox.js
+++ chapter_seven/sandbox.js
@@ -1,23 +1,15 @@
 const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  console.log(form.username.value);
+  
+  const username =  form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+  if(usernamePattern.test(username)){
+    feedback.textContent = 'that username is valid!'
+  } else {
+    feedback.textContent = 'username must contain only letters & be between 6 & 12 characters';
+  }
 });
-
-// testing RegEx
-
-const username = 'shaunyp';
-const pattern = /^[a-z]{6,}$/;
-
-// let result = pattern.test(username);
-
-// if(result){
-//   console.log('regex test passed :)');
-// } else {
-//   console.log('regex test failed :(');
-// }
-
-let result = username.search(pattern);
-
-console.log(result);
