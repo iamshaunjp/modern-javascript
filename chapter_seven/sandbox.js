c83cd9afc24e193cfd12a80d8495036b6951ddc5
Shaun <shaun@thenetninja.co.uk>
2019-02-16 09:58:49 +0000

    lesson-61

--- chapter_seven/sandbox.js
+++ chapter_seven/sandbox.js
@@ -1,9 +1,23 @@
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  // console.log('form submitted');
-  // console.log(username.value);
   console.log(form.username.value);
 });
+
+// testing RegEx
+
+const username = 'shaunyp';
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username);
+
+// if(result){
+//   console.log('regex test passed :)');
+// } else {
+//   console.log('regex test failed :(');
+// }
+
+let result = username.search(pattern);
+
+console.log(result);
