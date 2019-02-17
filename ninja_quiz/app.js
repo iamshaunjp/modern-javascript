3eaaa6bf748e2a3f8560ccddf688bd48119af8a9
Shaun <shaun@thenetninja.co.uk>
2019-02-17 08:51:07 +0000

    lesson-70

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -17,24 +17,16 @@ form.addEventListener('submit', e => {
 
   // show the result
   scrollTo(0, 0);
-  result.querySelector('span').textContent = `${score}%`;
   result.classList.remove('d-none');
 
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output === score){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+
 });
-
-// window object (the global object)
-
-// console.log(window);
-
-// console.log('hello');
-// window.console.log('hello');
-
-// console.log(document.querySelector('form'));
-// console.log(window.document.querySelector('form'));
-
-// // alert('hello');
-// // window.alert('hello again');
-
-// setTimeout(() => {
-//   alert('hello, ninjas');
-// }, 3000);
