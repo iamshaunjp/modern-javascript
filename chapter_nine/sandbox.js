42125d00ec27bded9d697c231f06bfcb63ecc9b0
Shaun <shaun@thenetninja.co.uk>
2019-02-18 14:21:20 +0000

    lesson-73

--- chapter_nine/sandbox.js
+++ chapter_nine/sandbox.js
@@ -1,22 +1,26 @@
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+// const scores = [10, 20, 60, 40, 70, 90, 30];
 
-// const salePrices = prices.map(price => price / 2);
-// console.log(salePrices);
+// const result = scores.reduce((acc, curr) => {
+//   if(curr > 50){
+//     acc++;
+//   }
+//   return acc;
+// }, 0);
 
-const products = [
-  {name: 'gold star', price: 20},
-  {name: 'mushroom', price: 40},
-  {name: 'green shells', price: 30},
-  {name: 'banana skin', price: 10},
-  {name: 'red shells', price: 50}
+// console.log(result);
+
+const scores = [
+  {player: 'mario', score: 50},
+  {player: 'yoshi', score: 30},
+  {player: 'mario', score: 70},
+  {player: 'crystal', score: 60}
 ];
 
-const saleProducts = products.map(product => {
-  if(product.price > 30){
-    return {name: product.name, price: product.price / 2}
-  } else {
-    return product;
+const marioTotal = scores.reduce((acc, curr) => {
+  if(curr.player === 'mario'){
+    acc += curr.score;
   }
-});
+  return acc;
+}, 0);
 
-console.log(products, saleProducts);
+console.log(marioTotal);
