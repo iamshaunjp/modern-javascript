e65c2b7c562b18309aedea8955698e0084a7c972
Shaun <shaun@thenetninja.co.uk>
2019-02-18 12:36:29 +0000

    lesson-72

--- chapter_nine/sandbox.js
+++ chapter_nine/sandbox.js
@@ -1,14 +1,22 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-// const highScores = scores.filter(score => score > 20);
-// console.log(highScores);
+// const salePrices = prices.map(price => price / 2);
+// console.log(salePrices);
 
-const users = [
-  {name: 'shaun', premium: true},
-  {name: 'yoshi', premium: false},
-  {name: 'mario', premium: false},
-  {name: 'chun-li', premium: true}
+const products = [
+  {name: 'gold star', price: 20},
+  {name: 'mushroom', price: 40},
+  {name: 'green shells', price: 30},
+  {name: 'banana skin', price: 10},
+  {name: 'red shells', price: 50}
 ];
 
-const premiumUsers = users.filter(user => user.premium);
-console.log(premiumUsers);
+const saleProducts = products.map(product => {
+  if(product.price > 30){
+    return {name: product.name, price: product.price / 2}
+  } else {
+    return product;
+  }
+});
+
+console.log(products, saleProducts);
