7f4074e27a5372a8f3a690625dd2ef39783ee484
Shaun <shaun@thenetninja.co.uk>
2019-02-18 14:24:28 +0000

    lesson-74

--- chapter_nine/sandbox.js
+++ chapter_nine/sandbox.js
@@ -1,26 +1,4 @@
-// const scores = [10, 20, 60, 40, 70, 90, 30];
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-// const result = scores.reduce((acc, curr) => {
-//   if(curr > 50){
-//     acc++;
-//   }
-//   return acc;
-// }, 0);
-
-// console.log(result);
-
-const scores = [
-  {player: 'mario', score: 50},
-  {player: 'yoshi', score: 30},
-  {player: 'mario', score: 70},
-  {player: 'crystal', score: 60}
-];
-
-const marioTotal = scores.reduce((acc, curr) => {
-  if(curr.player === 'mario'){
-    acc += curr.score;
-  }
-  return acc;
-}, 0);
-
-console.log(marioTotal);
+const firstHighScore = scores.find(score => score > 50);
+console.log(firstHighScore);
