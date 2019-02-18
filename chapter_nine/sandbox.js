2ecefac5b0ec6ef3b0c83c1b69ac2bd6237f6bd9
Shaun <shaun@thenetninja.co.uk>
2019-02-18 14:38:04 +0000

    lesson-75

--- chapter_nine/sandbox.js
+++ chapter_nine/sandbox.js
@@ -1,4 +1,37 @@
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+// example 1 - sorting strings
+const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
 
-const firstHighScore = scores.find(score => score > 50);
-console.log(firstHighScore);
+// names.sort();
+names.reverse();
+console.log(names);
+
+// example 2 - sorting numbers
+const scores = [10, 50, 20, 5, 35, 70, 45];
+
+// scores.sort();
+scores.reverse();
+console.log(scores);
+
+// example 3 - sorting objects
+const players = [
+  {name: 'mario', score: 20},
+  {name: 'luigi', score: 10},
+  {name: 'chun-li', score: 50},
+  {name: 'yoshi', score: 30},
+  {name: 'shaun', score: 70}
+];
+
+// players.sort((a,b) => {
+//   if(a.score > b.score){
+//     return -1;
+//   } else if (b.score > a.score){
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// });
+
+players.sort((a,b) => b.score - a.score);
+
+console.log(players);
+
