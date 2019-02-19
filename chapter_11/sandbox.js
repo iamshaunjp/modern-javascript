a91f39e53753630359532b325a0eeb4b0ccfb88a
Shaun <shaun@thenetninja.co.uk>
2019-02-19 16:40:10 +0000

    lesson-83

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,21 +1,20 @@
+//const before = new Date('02/01/2019 7:30:59');
+const before = new Date('February 1 2019 7:30:59');
 const now = new Date();
 
-console.log(now);
-console.log(typeof now);
+const diff = now.getTime() - before.getTime();
+// const diff = now - before;
 
-// years, months, days, times
-console.log('getFullYear:', now.getFullYear());
-console.log('getMonth (0-based):', now.getMonth());
-console.log('getDate:', now.getDate());
-console.log('getDay (0-based):', now.getDay());
-console.log('getHours:', now.getHours());
-console.log('getMinutes:', now.getMinutes());
-console.log('getSeconds:', now.getSeconds());
+console.log(diff);
 
-// timestamps
-console.log('timestamp:', now.getTime());
+const mins = Math.round(diff / 1000 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
 
-// date strings
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());
+console.log(`the before date was ${mins} mins ago`);
+console.log(`the before date was ${hours} hours ago`);
+console.log(`the before date was ${days} days ago`);
+
+// converting timestamps to dates
+const timestamp = 1675938474990;
+console.log(new Date(timestamp));
