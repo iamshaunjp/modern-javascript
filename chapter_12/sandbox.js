868d9ca4c221dc038c87a5e373c1792d2ea12f3a
Shaun <shaun@thenetninja.co.uk>
2019-02-21 17:17:57 +0000

    lesson-96

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,33 +1,10 @@
-const getTodos = (resource) => {
+// fetch API
 
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-  
-      if(request.readyState === 4 && request.status === 200){
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4){
-        reject('could not fetch the data');
-      }
-  
-    });
-    
-    request.open('GET', resource);
-    request.send();
+fetch('json/luigi.json').then(response => {
+    //console.log(response);
+    return response.json();
+  }).then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.log(err);
   });
-
-};
-
-getTodos('json/luigi.json').then(data => {
-  console.log('promise 1 resolved:', data);
-  return getTodos('json/mario.json');
-}).then(data => {
-  console.log('promise 2 resolved:', data);
-  return getTodos('json/shaun.json');
-}).then(data => {
-  console.log('promise 3 resolved:', data);
-}).catch(err => {
-  console.log('promise rejected:', err);
-});
