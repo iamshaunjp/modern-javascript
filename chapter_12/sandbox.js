8ce51bef8b8ab52e7fd4ef50983aa861d46f3fc0
Shaun <shaun@thenetninja.co.uk>
2019-02-21 16:03:32 +0000

    lesson-90

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,11 +1,12 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-  // console.log(request, request.readyState);
-  if(request.readyState === 4){
-    // console.log(request);
+  // console.log(request);
+  if(request.readyState === 4 && request.status === 200){
     console.log(request.responseText);
-  } 
+  } else if (request.readyState === 4){
+    console.log('could not fetch the data');
+  }
 });
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
