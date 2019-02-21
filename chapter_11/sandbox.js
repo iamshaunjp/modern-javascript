0a5f767db67e58afedbd926212b2d071926c785a
Shaun <shaun@thenetninja.co.uk>
2019-02-21 15:32:38 +0000

    lesson-89

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,9 +1,14 @@
-console.log(1);
-console.log(2);
+const request = new XMLHttpRequest();
 
-setTimeout(() => {
-  console.log('callback function fired');
-}, 2000);
+request.addEventListener('readystatechange', () => {
+  // console.log(request, request.readyState);
+  if(request.readyState === 4){
+    // console.log(request);
+    console.log(request.responseText);
+  } else if(request.readyState === 4) {
+    console.log('error with request');
+  }
+});
 
-console.log(3);
-console.log(4);
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
