e61deeb8a48e635ffdf602e70c879abceedf6688
Shaun <shaun@thenetninja.co.uk>
2019-02-21 16:12:03 +0000

    lesson-92

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -5,14 +5,15 @@ const getTodos = (callback) => {
   request.addEventListener('readystatechange', () => {
 
     if(request.readyState === 4 && request.status === 200){
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4){
       callback('could not fetch the data', undefined);
     }
 
   });
   
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.open('GET', 'todos.json');
   request.send();
 
 };
