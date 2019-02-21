27777e1e466b20796ce673563a2df60c6fc61ec4
Shaun <shaun@thenetninja.co.uk>
2019-02-21 17:28:32 +0000

    lesson-98

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -3,18 +3,16 @@
 const getTodos = async () => {
 
   let response = await fetch('json/luigi.json');
+
+  if(response.status !== 200){
+    throw new Error('cannot fetch the data');
+  }
+
   let data = await response.json();
   return data;
 
 };
 
-console.log(1);
-console.log(2);
-
 getTodos()
-  .then(data => console.log('resolved:', data));
-
-console.log(3);
-console.log(4);
-
-// console.log(getTodos());
+  .then(data => console.log('resolved:', data))
+  .catch(err => console.log('rejected:', err.message));
