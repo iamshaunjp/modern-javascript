2559ac82464296416609455060aa0040c4c372d1
Shaun <shaun@thenetninja.co.uk>
2019-02-21 16:07:26 +0000

    lesson-91

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,13 +1,33 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
 
-request.addEventListener('readystatechange', () => {
-  // console.log(request);
-  if(request.readyState === 4 && request.status === 200){
-    console.log(request.responseText);
-  } else if (request.readyState === 4){
-    console.log('could not fetch the data');
+  const request = new XMLHttpRequest();
+
+  request.addEventListener('readystatechange', () => {
+
+    if(request.readyState === 4 && request.status === 200){
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4){
+      callback('could not fetch the data', undefined);
+    }
+
+  });
+  
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.send();
+
+};
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+  console.log('callback function fired');
+  if(err){
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
