14efdc5a363f9e351041efff4f045978ad3642b9
Shaun <shaun@thenetninja.co.uk>
2019-02-25 11:30:15 +0000

    lesson-109

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -22,12 +22,7 @@ const updateUI = (data) => {
   const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
   icon.setAttribute('src', iconSrc);
   
-  let timeSrc = null;
-  if(weather.IsDayTime){
-    timeSrc = 'img/day.svg';
-  } else {
-    timeSrc = 'img/night.svg';
-  }
+  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
   time.setAttribute('src', timeSrc);
 
   // remove the d-none class if present
