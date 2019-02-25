275114dd9b762e81751e58071911ef270a3c1e7d
Shaun <shaun@thenetninja.co.uk>
2019-02-25 11:09:37 +0000

    lesson-106

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,4 +1,27 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // remove the d-none class if present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
+};
 
 const updateCity = async (city) => {
 
@@ -18,6 +41,6 @@ cityForm.addEventListener('submit', e => {
 
   // update the ui with new city
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
