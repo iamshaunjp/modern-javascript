92180305d3a2f16265ef2057bc3d7aeb872315a0
Shaun <shaun@thenetninja.co.uk>
2019-02-25 14:31:58 +0000

    lesson-114

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -51,4 +51,14 @@ cityForm.addEventListener('submit', e => {
   updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
+
+  // set local storage
+  localStorage.setItem('city', city);
+
 });
+
+if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
