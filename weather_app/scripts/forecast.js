dbc38fe3572eb0f056002aad9dfd443d91987925
Shaun <shaun@thenetninja.co.uk>
2019-02-25 10:49:05 +0000

    lesson-103

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,5 +1,18 @@
 const key = '	xvpOBAAypFh84YftzPvUCh8ZM80gbYIG';
 
+// get weather information
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
 // get city information
 const getCity = async (city) => {
 
@@ -13,6 +26,8 @@ const getCity = async (city) => {
 
 };
 
-getCity('manchester')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+getCity('manchester').then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+  console.log(data);
+}).catch(err => console.log(err));
