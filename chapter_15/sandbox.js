47c21bc0e85a561f07a59e14c02deac4f6fccd2a
Shaun <shaun@thenetninja.co.uk>
2019-02-28 15:12:34 +0000

    lesson-131

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
 
 const addRecipe = (recipe) => {
   let time = recipe.created_at.toDate();
@@ -12,12 +13,28 @@ const addRecipe = (recipe) => {
   list.innerHTML += html;
 };
 
+// get documents
 db.collection('recipes').get().then(snapshot => {
-  // console.log(snapshot);
   snapshot.docs.forEach(doc => {
-    // console.log(doc.data());
     addRecipe(doc.data());
   });
 }).catch(err => {
   console.log(err);
+});
+
+// save documents
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now)
+  };
+
+  db.collection('recipes').add(recipe).then(() => {
+    console.log('recipe added');
+  }).catch(err => {
+    console.log(err);
+  });
 });
