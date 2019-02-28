7198799b7c05cd71477e6ccdde0a5f07e5c384f3
Shaun <shaun@thenetninja.co.uk>
2019-02-28 08:44:32 +0000

    lesson-119

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,17 +1,33 @@
 class User {
   constructor(username, email){
-    // this.username = 'mario';
     this.username = username;
     this.email = email;
+    this.score = 0;
+  }
+  login(){
+    console.log(`${this.username} just logged in`);
+    return this;
+  }
+  logout(){
+    console.log(`${this.username} just logged out`);
+    return this;
+  }
+  incScore(){
+    this.score += 1;
+    console.log(`${this.username} has a score of ${this.score}`);
+    return this;
   }
 }
 
 const userOne = new User('luigi', 'luigi@thenetninja.co.uk');
 const userTwo = new User('mario', 'mario@thenetninja.co.uk');
 
-console.log(userOne, userTwo);
+// userOne.login();
+// userOne.logout();
+// userTwo.login();
+// userTwo.logout();
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+userTwo.login()
+  .incScore()
+  .incScore()
+  .logout();
