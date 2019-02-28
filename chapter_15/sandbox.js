4d6220a066ad1cf9b9f7ce0f0f2f8ebda967004c
Shaun <shaun@thenetninja.co.uk>
2019-02-28 08:16:38 +0000

    lesson-117

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,26 +1,3 @@
-const userOne = {
-  username: 'ryu',
-  email: 'ryu@thenetninja.co.uk',
-  login(){
-    console.log('the user logged in');
-  },
-  logout(){
-    console.log('the user logged out');
-  }
-};
-
-console.log(userOne.email, userOne.username);
-userOne.login();
-
-const userTwo = {
-  username: 'chun-li',
-  email: 'chun.li@thenetninja.co.uk',
-  login(){
-    console.log('the user logged in');
-  },
-  logout(){
-    console.log('the user logged out');
-  }
-};
-
-// const userThree = new User('shaun', 'shaun@thenetninja.co.uk');
+class User {
+  
+}
