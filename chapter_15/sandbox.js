48a0595418ed5b1a8d6bcfcf0566d6ac602fc23c
Shaun <shaun@thenetninja.co.uk>
2019-02-28 10:01:40 +0000

    lesson-123

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,25 +1,21 @@
-// constructor functions
-// class User {
-//   constructor(username){
-//     this.username = username;
-//   }
-// }
-
 function User(username, email){
   this.username = username;
   this.email = email;
-  this.login = function(){
-    console.log(`${this.username} has logged in`);
-  };
 }
+
+User.prototype.login = function(){
+  console.log(`${this.username} has logged in`);
+  return this;
+};
+
+User.prototype.logout = function(){
+  console.log(`${this.username} has logged out`);
+  return this;
+};
 
 const userOne = new User('ryu', 'ryu@thenetninja.co.uk');
 const userTwo = new User('chun-li', 'chun.li@thenetninja.co.uk');
 
-console.log(userOne, userTwo);
-userOne.login();
+console.log(userOne);
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+userOne.login().logout();
