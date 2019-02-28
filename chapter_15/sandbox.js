dc148eaed22cb051a69483ca88a44a1fcce9f76e
Shaun <shaun@thenetninja.co.uk>
2019-02-28 09:33:33 +0000

    lesson-121

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -20,6 +20,10 @@ class User {
 }
 
 class Admin extends User {
+  constructor(username, email, title){
+    super(username, email);
+    this.title = title;
+  }
   deleteUser(user){
     users = users.filter(u => u.username !== user.username);
     return this; // allow method chaining
@@ -28,12 +32,6 @@ class Admin extends User {
 
 const userOne = new User('luigi', 'luigi@thenetninja.co.uk');
 const userTwo = new User('mario', 'mario@thenetninja.co.uk');
-const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk');
+const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk', 'black-belt ninja');
 
 console.log(userOne, userThree);
-
-let users = [userOne, userTwo, userThree];
-console.log(users);
-
-userThree.deleteUser(userTwo);
-console.log(users);
