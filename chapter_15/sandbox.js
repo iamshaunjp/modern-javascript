94ad5748ff01ec7e74baca253f5698eb655e6d81
Shaun <shaun@thenetninja.co.uk>
2019-02-28 08:27:12 +0000

    lesson-118

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,3 +1,17 @@
 class User {
-  
+  constructor(username, email){
+    // this.username = 'mario';
+    this.username = username;
+    this.email = email;
+  }
 }
+
+const userOne = new User('luigi', 'luigi@thenetninja.co.uk');
+const userTwo = new User('mario', 'mario@thenetninja.co.uk');
+
+console.log(userOne, userTwo);
+
+// the 'new' keyword
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
