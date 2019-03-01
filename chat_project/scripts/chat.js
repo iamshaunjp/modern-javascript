bec013e0e103f03103a3670461a6b4dac2ab84a5
Shaun <shaun@thenetninja.co.uk>
2019-03-01 11:48:07 +0000

    lesson-138

--- chat_project/scripts/chat.js
+++ chat_project/scripts/chat.js
@@ -1,3 +1,8 @@
+// adding new chat documents
+// setting up a real-time listener to get new chats
+// updating the username
+// updating the room
+
 class Chatroom {
   constructor(room, username){
     this.room = room;
