121a83750c813a031fcf89dee6b4796dd6bbaaa3
Shaun <shaun@thenetninja.co.uk>
2019-03-01 14:58:58 +0000

    lesson-143

--- chat_project/scripts/ui.js
+++ chat_project/scripts/ui.js
@@ -6,11 +6,15 @@ class ChatUI {
     this.list = list;
   }
   render(data){
+    const when = dateFns.distanceInWordsToNow(
+      data.created_at.toDate(),
+      { addSuffix:true }
+    );
     const html = `
       <li class="list-group-item">
         <span class="username">${data.username}</span>
         <span class="message">${data.message}</span>
-        <div class="time">${data.created_at.toDate()}</span>
+        <div class="time">${when}</span>
       </li>
     `;
     this.list.innerHTML += html;
