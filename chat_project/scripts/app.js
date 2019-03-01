116e688d63032625e9c1145d6d6848d9c3fc01d8
Shaun <shaun@thenetninja.co.uk>
2019-03-01 15:10:05 +0000

    lesson-143

--- chat_project/scripts/app.js
+++ chat_project/scripts/app.js
@@ -1,15 +1,5 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
-const newChatForm = document.querySelector('.new-chat');
-
-// add a new chat
-newChatForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const message = newChatForm.message.value.trim();
-  chatroom.addChat(message)
-    .then(() => newChatForm.reset())
-    .catch(err => console.log(err));
-});
 
 // class instances
 const chatUI = new ChatUI(chatList);
