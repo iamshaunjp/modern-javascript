91e05ef34044c5c575ec3bc13bba56626d8880b8
Mamta Gupta <mamta.sibbal@gmail.com>
2020-04-16 17:52:01 -0700

    my changes in chapter-7

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,7 +1,7 @@
-const form = document.querySelector('.signup-form');
+const form = document.querySelector(".signup-form");
 // const username = document.querySelector('#username');
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   // console.log('form submitted');
   // console.log(username.value);
