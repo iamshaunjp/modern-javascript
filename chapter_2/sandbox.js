a005fe3243af0f4422e54e57be8fbd6b60a2bf15
eddiepmonark <53535274+eddiepmonark@users.noreply.github.com>
2021-02-12 09:37:49 -0800

    testing js stuff

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,175 @@
+// alert('hello, world');
+
+// let age = 25;
+// let email = 'eddie@e11world.com';
+// let firstName = 'Eddie';
+// let lastName = 'Potros';
+// let fullName = firstName + ' ' + lastName;
+
+
+// console.log(age);
+// console.log(firstName + ' ' + lastName);
+// console.log(email[0]);
+
+// console.log(fullName.toUpperCase());
+// console.log(email.indexOf('@'));
+// console.log(email.lastIndexOf('1'));
+// console.log(email.slice(0,10));
+// console.log(email.substr(0,10));
+// console.log(email.replace('d', 'D'));
+
+
+
+// order of operation B I D M A S ( bracket indiceis division multiplication addition subtraction )
+
+// math operators + - * / ** %
+// Operator	Description
+// +	    Addition
+// -	    Subtraction
+//- *	    Multiplication
+//- **	    Exponentiation (ES2016)
+// /	    Division
+// %	    Modulus (Remainder)
+// ++	    Increment
+// --	    Decrement
+
+// let radius = 10;
+// const pi = 3.14;
+// let result = radius % 3;
+// let result = pi * radius ** 2;
+
+// console.log(radius, pi);
+
+// console.log(10 / 2);
+
+// let likes = 10;
+// likes = likes + 1;
+// likes++;
+// likes--;
+// likes +=10;
+// likes -=5;
+// likes /=2;
+// likes *=2;
+
+// NaN = not a number
+// console.log(5 * 'hello');
+
+// let result = 'the blog has ' + likes + ' likes';
+// const title = 'Best reads of 2019';
+// const author = 'Mario';
+// const likes = 30;
+
+// console.log(result);
+
+// concatenation way
+// let result = 'The blog called ' + title + ' by ' + author;
+
+// template string way
+// let result = `The blog called ${title} by ${author}`;
+
+// creating html templates
+// let html = `<h2>${title}</h2>
+{/* <p>By ${author}</p> */}
+// `;
+
+// console.log(html);
+
+
+
+// let ninjas = ['eddie', 'mike', 'shawn'];
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 33];
+// console.log(ages[2]);
+
+// let random = ['eddie', 'potros', 37];
+// console.log(random[2]);
+
+// array methods
+
+// let result = ninjas.join(', ');
+// let result = ninjas.indexOf('eddie');
+// let result = ninjas.concat(['ken', 'sugar']);
+// let result = ninjas.push('ken');
+// result = ninjas.pop();
+// console.log(result);
+// console.log(ninjas);
+
+
+
+// let age;
+// console.log(age, age + 3, `the age is ${age}`);
+//             undefined NaN "the age is undefined"
+
+// let age = null;
+// console.log(age, age + 3, `the age is ${age}`);
+//         null 3 "the age is null"
+
+
+// let names = ['eddie', 'potros'];
+// let age = 37;
+
+// let result = names.includes('eddie');
+// console.log(result);
+
+// comparison operators
+// console.log(age == 37);
+// console.log(age == 27);
+// console.log(age != 33);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age >= 20);
+// console.log(age <= 20);
+
+
+// let fName = 'Eddie';
+// lower case letter is greater than uppercase
+// console.log(fName == 'Eddie');
+// console.log(fName == 'eddie');
+// console.log(fName > 'ayad');
+// console.log(fName > 'Ayad');
+
+// loose comparison different type (string/number) can still be equal
+// console.log(age == '37');
+// console.log(age != 37);
+// console.log(age != '37');
+
+// // strict comparison (different types cannot be equal)
+// console.log(age === 37);
+// console.log(age === '37');
+// console.log(age !== 37);
+// console.log(age !== '37');
+
+
+
+// type conversion
+// let score = '100';
+// if left as is, result is 1001
+// score = Number(score);
+// here we get the value of 100 + 1 = 101
+
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+// let result = Boolean('');
+// console.log(result, typeof result);
+
+
+
+
+
+
+
+
+
+/*
+nfn
+sfc
+clg
+ctrl+shift+l for #
+*/
+
