1006f265b87cc7251db38bda353174736b2ff935
Evelyn Z <evelynzdragon@gmail.com>
2021-09-16 14:06:11 -0700

    created updateUI function to receive the data from updateCity function, and them display them on the screen; not displaying the UI until it receives data

--- chapter_13_weather_app/scripts/app.js
+++ chapter_13_weather_app/scripts/app.js
@@ -4,7 +4,28 @@ const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const  details = document.querySelector('.details');
 
+const updateUI = (data) => {
 
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+  // console.log(cityDetails, weather)
+
+  //update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather[0].Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  //remove the d-none class if present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none')
+  }
+
+}
 
 const updateCity = async (city) => {
   
@@ -32,6 +53,6 @@ cityForm.addEventListener('submit', e=>{
 
   //update the ui with new city
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err))
 })
