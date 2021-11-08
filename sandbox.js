30b4902ac19a52ce8104369d81d0a4ee84c2ee8a
pambonian <pambonian@gmail.com>
2021-11-08 11:32:48 -0500

    practice

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+let age = 25;
+let year = 2021;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log("Score: " + score);
